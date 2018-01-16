c32900108912297c69391a809ce5bcfce728f3b1
liqiang8 <liqiang8@xiaomi.com>
2018-01-16 17:38:56 +0800

    加入druid-start

---
+++ src/main/java/com/liqiang/springboottest/DruidConfig.java
@@ -0,0 +1,26 @@
+package com.liqiang.springboottest;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * @author liqiang (liqiang8@xiaomi.com)
+ * @Date 2018/1/16
+ **/
+@Configuration
+public class DruidConfig {
+    private static Logger logger= LoggerFactory.getLogger(DruidConfig.class);
+
+
+    @Bean
+    @Primary
+    public DataSource dataSource(){
+        return DruidDataSourceBuilder.create().build();
+    }
+}
--- src/main/java/com/liqiang/springboottest/SpringBootTestApplication.java
+++ src/main/java/com/liqiang/springboottest/SpringBootTestApplication.java
@@ -2,8 +2,10 @@ package com.liqiang.springboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+//@ServletComponentScan
 public class SpringBootTestApplication {
 
 	public static void main(String[] args) {
