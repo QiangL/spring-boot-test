69d7ea266c553469fc301b618d90e13fd3c61b3a
liqiang8 <liqiang8@xiaomi.com>
2018-01-17 20:31:44 +0800

    尝试使用druid的监控功能

--- src/main/java/com/liqiang/springboottest/config/DruidConfig.java
+++ src/main/java/com/liqiang/springboottest/config/DruidConfig.java
@@ -1,13 +1,17 @@
 package com.liqiang.springboottest.config;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Druid配置
