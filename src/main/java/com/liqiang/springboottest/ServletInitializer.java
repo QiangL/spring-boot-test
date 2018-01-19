5e2915e89455d09fcd55c57a3fcd859e2143e9f4
liqiang8 <liqiang8@xiaomi.com>
2018-01-19 11:54:47 +0800

    增加SpringBootServletInitializer的说明

--- src/main/java/com/liqiang/springboottest/ServletInitializer.java
+++ src/main/java/com/liqiang/springboottest/ServletInitializer.java
@@ -3,8 +3,14 @@ package com.liqiang.springboottest;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+/**
+ * 创建war包
+ * https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#howto-create-a-deployable-war-file
+ * https://stormpath.com/blog/tutorial-spring-boot-war-files#setting-up-a-servlet
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
+    //TODO 尝试注掉这个类，看打war包的问题
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(SpringBootTestApplication.class);
