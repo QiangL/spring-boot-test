097637193c490f7d0847cdcfda419a0cf0c421bc
liqiang8 <liqiang8@xiaomi.com>
2018-01-22 20:02:36 +0800

    update test

--- spring-boot-test-module/src/main/java/com/liqiang/springboot/module/ModuleConfig.java
+++ spring-boot-test-module/src/main/java/com/liqiang/springboot/module/ModuleConfig.java
@@ -1,21 +1,19 @@
 package com.liqiang.springboot.module;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author liqiang
  * @Date 2018/1/19
  **/
 @SpringBootApplication
-@PropertySource({"classpath:application-module.properties"})
 @MapperScan("com.liqiang.springboot.module.mapper")
 public class ModuleConfig {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ModuleConfig.class, args);
-    }
 }
--- spring-boot-test-service/src/main/java/com/liqiang/springboot/service/ServiceConfig.java
+++ spring-boot-test-service/src/main/java/com/liqiang/springboot/service/ServiceConfig.java
@@ -1,23 +1,17 @@
 package com.liqiang.springboot.service;
 
 import com.liqiang.springboot.module.ModuleConfig;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author liqiang
  * @Date 2018/1/19
  **/
-@SpringBootApplication(scanBasePackages = "com.liqiang.springboot")
-@PropertySource({"classpath:application-service.properties"})
+@SpringBootApplication
+@PropertySource({"classpath:application.properties"})
 @ImportAutoConfiguration(ModuleConfig.class)
 public class ServiceConfig {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ServiceConfig.class, args);
-    }
 }
